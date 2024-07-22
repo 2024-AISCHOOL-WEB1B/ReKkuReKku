ebfb06000417a1948f8c2f2a5ccb037c9d5c2973
gonaeun <gonaeunn@gmail.com>
2024-07-22 14:29:31 +0900

    db연결 수정2

--- pro/model/users.js
+++ pro/model/users.js
@@ -1,9 +1,14 @@
 const conn = require("./db");
 
 
-// 모든 사용자 조회
-const user = { 
-}
+// 모든 사용자 정보 조회
+const user = {
+    getAll: (callback) =>{
+    conn.query("SELECT * FROM user", callback);},
+    // 쿼리 실행 후, callback 함수를 실행하여 sql문 결과를 불러옴
+
+
+};
 
 
 module.exports = user;
