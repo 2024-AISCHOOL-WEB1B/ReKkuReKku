1cc4d9317e9c29109ea2596db86d603230393dfa
gonaeun <gonaeunn@gmail.com>
2024-07-23 12:34:00 +0900

    db에 회원가입정보저장 쿼리문 작성, 회원가입 라우터 생성 및 메인라우터 주석추가

--- pro/routes/mainRouter.js
+++ pro/routes/mainRouter.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const session = require('./session');
-const port = 4000;
+const port = 4000;  // 4000포트 오픈
 const cors = require("cors");
 
-// CORS 설정
+// CORS(Cross-Origin Resource Sharing) 설정
+// npm i CORS 설치 필요
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3000',   // 3000포트의 데이터를 4000포트로 보냄
 }));
 
+// 미들웨어 설정
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(session);
@@ -17,10 +19,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-//  라우터 예시
- const loginRouter = require('./routes/loginRouter');
+
+// 회원가입 라우터 설정
+ const joinRouter = require('./joinRouter'); 
+ app.use('/user/join', joinRouter);
+
+// 로그인 라우터 설정
+ const loginRouter = require('./loginRouter');
  app.use('/user/login', loginRouter);
 
+// 서버 시작
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
